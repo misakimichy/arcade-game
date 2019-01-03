4510fd8c78fd4da4c6caf0432f3dbf7b31ae7e82
Misaki Koonce <misaki.koonce@gmail.com>
2019-01-03 13:11:58 -0800

    player move after keypress event

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -25,6 +26,7 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// Player doesn't need speed because player moves depends on the keypress event
 const Player = function(x, y) {
     this.x = x;
     this.y = y;
@@ -39,9 +41,25 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// player moves each grid when keypress happens
 Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'left' && this.x > 0){
-        
+        this.x -= 101;
+    }
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    }
+    if (keyPress == 'right' && this.x < 405){
+        this.x += 101;
+    }
+    if (keyPress == 'down' && this.y < 405) {
+        this.y += 83;
+    }
+    //when player got to the top, the new game starts
+    if (this.y == 0) {
+        setTimeout(function() {
+            new Player(200, 400);
+        }, 500);
     }
 };
 
@@ -50,9 +68,10 @@ Player.prototype.handleInput = function(keyPress) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [new Enemy()];
+const enemyLocation = [83, 166, 249];
 // Place the player object in a variable called player
 // provide the original position x and y
-const player = new Player(202, 405);
+const player = new Player(200, 400);
 
 
 // This listens for key presses and sends the keys to your
